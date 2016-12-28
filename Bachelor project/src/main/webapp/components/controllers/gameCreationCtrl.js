82cbf841929150581537585dcace760017376ade
Jan Čech <cech@vrk.cz>
2016-12-28 05:17:19 +0100

    add gameCreation.js

---
+++ Bachelor project/src/main/webapp/components/controllers/gameCreationCtrl.js
@@ -0,0 +1,47 @@
+'use strict';
+
+gameCreationCtrl
+    .controller('gameCreationCtrl', function ($scope, gameService, loginService, locationService) {
+
+        $scope.maxPlayers = maxPlayers;
+        $scope.playersList = playersList;
+        $scope.creator = false;
+
+        $scope.startGame = startGame;
+
+        var maxPlayers = 1;
+        var owner = null;
+        var playersList;
+
+        init();
+
+        function init() {
+
+            loginService.getUser()
+                .then(function (user) {
+                    gameService.getPlayersGame(user)
+                        .then(function (data) {
+                            console.log(data);
+                            maxPlayers = data.maxPlayersCount;
+                            owner = data.owner;
+                            playersList = data.playersList;
+                            $scope.maxPlayers = maxPlayers;
+                            $scope.playersList = playersList;
+
+                            if (user === owner) {
+                                $scope.creator = true;
+                            } else {
+                                $scope.creator = false;
+                            }
+                        }
+                    )
+                }
+            );
+        }
+
+        function startGame(){
+            locationService.goToGame();
+        }
+    }
+
+);
