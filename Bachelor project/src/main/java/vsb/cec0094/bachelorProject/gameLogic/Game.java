02a13e39d9f98ae6e56c94bc57c7f5cd0519c577
Jan Čech <cech@vrk.cz>
2017-02-26 10:34:27 +0100

    relocate all game functions to services

--- Bachelor project/src/main/java/vsb/cec0094/bachelorProject/gameLogic/Game.java
+++ Bachelor project/src/main/java/vsb/cec0094/bachelorProject/gameLogic/Game.java
@@ -4,16 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import vsb.cec0094.bachelorProject.exceptions.InvalidActionException;
 import vsb.cec0094.bachelorProject.exceptions.TooExpensiveExpeditionException;
 import vsb.cec0094.bachelorProject.exceptions.TwoShipsOfSameTypeOnTableException;
-import vsb.cec0094.bachelorProject.gameLogic.card.Card;
-import vsb.cec0094.bachelorProject.gameLogic.card.CardType;
-import vsb.cec0094.bachelorProject.gameLogic.card.Expedition;
 import vsb.cec0094.bachelorProject.gameLogic.pack.DrawPile;
 import vsb.cec0094.bachelorProject.gameLogic.pack.Expeditions;
 import vsb.cec0094.bachelorProject.gameLogic.pack.Table;
 import vsb.cec0094.bachelorProject.gameLogic.services.FaceCardService;
+import vsb.cec0094.bachelorProject.gameLogic.services.GameUtilsService;
 import vsb.cec0094.bachelorProject.gameLogic.services.GetCardFromTableService;
-import vsb.cec0094.bachelorProject.models.Action;
-import vsb.cec0094.bachelorProject.models.ActionToShow;
+import vsb.cec0094.bachelorProject.gameLogic.services.PlayerSwittchingService;
 import vsb.cec0094.bachelorProject.models.GameInQueue;
 
 import java.io.Serializable;
@@ -27,6 +24,11 @@ public class Game implements Cloneable, Serializable {
     private final FaceCardService faceCardService = new FaceCardService(this);
     @JsonIgnore
     private final GetCardFromTableService getCardFromTableService = new GetCardFromTableService(this);
+    @JsonIgnore
+    private final GameUtilsService gameUtilsService = new GameUtilsService(this);
+    @JsonIgnore
+    private final PlayerSwittchingService playerSwittchingService = new PlayerSwittchingService(this);
+
     private String owner;
     private List<Player> players;
     private Table table;
@@ -40,6 +42,7 @@ public class Game implements Cloneable, Serializable {
     private Boolean gameOver;
     @JsonIgnore
     private DrawPile drawPile;
+
 
     public Game(GameInQueue gameInQueue) {
         gameOver = false;
@@ -57,7 +60,7 @@ public class Game implements Cloneable, Serializable {
                 .map(p -> new Player(p.getLogin()))
                 .collect(Collectors.toList());
         try {
-            recalculateShips();
+            gameUtilsService.recalculateShips();
         } catch (TwoShipsOfSameTypeOnTableException e) {
             //can not occur
             e.printStackTrace();
@@ -76,54 +79,15 @@ public class Game implements Cloneable, Serializable {
     }
 
     public void applyAdmiral() {
-        if (table.getCountOfCards() > 4) {
-            admiralApplied = true;
-            phase = Phase.TRADING;
-            Player activeP = players.get(activePlayer);
-            activeP.setCoins(activeP.getCoins() * activeP.getAdmiralsCount() * 2);
-        }
+        gameUtilsService.applyAdmiral();
     }
 
     public void skipAction() {
-        shiftPlayer(false);
+        playerSwittchingService.shiftPlayer(false);
     }
 
     public ActionAndSemiStateHolder playerGetCardFromTable(int cardPosition) throws InvalidActionException, TooExpensiveExpeditionException, CloneNotSupportedException {
-        if (Phase.EXPLORING.equals(phase)) {
-            phase = Phase.TRADING;
-        }
-        ActionToShow actionToShow = new ActionToShow(Action.GET_CARD, Table.TABLE, cardPosition);
-        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder(this, actionToShow);
-
-        Card card = table.getCards().remove(cardPosition);
-        cardTypeValidation(card);
-
-        if (activePlayer != playerOnTurn) {
-            players.get(playerOnTurn).addCoin();
-        }
-
-        if (Card.shipTypes.contains(card.getCardType())) {
-
-            Player playerDoingAction = players.get(activePlayer);
-            playerDoingAction.getCoinsFromShip(card);
-            drawPile.getUsedCard(card);
-
-            if (--cardsToTake == 0) {
-                shiftPlayer(false);
-            }
-            return actionAndSemiStateHolder;
-        } else {
-            Player playerDoingAction = players.get(activePlayer);
-            playerDoingAction.takeCharacterCard(card, true);
-            playerDoingAction.updateVariables();
-
-            if (--cardsToTake == 0) {
-                shiftPlayer(false);
-            }
-            actionAndSemiStateHolder.addState(this, new ActionToShow(Action.GET_CARD, playerDoingAction.getLogin(), playerDoingAction.getCards().indexOf(card)));
-            return actionAndSemiStateHolder;
-        }
-
+        return getCardFromTableService.playerGetCardFromTable(cardPosition);
     }
 
     public ActionAndSemiStateHolder faceCard() throws CloneNotSupportedException, InvalidActionException {
@@ -131,207 +95,7 @@ public class Game implements Cloneable, Serializable {
     }
 
     public ActionAndSemiStateHolder pickExpedition(int id) throws TooExpensiveExpeditionException, CloneNotSupportedException {
-        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder();
-        Expedition expedition = expeditions.getExpedition(id);
-        players.get(activePlayer).canTakeExpedition(expedition); // if not throws exeption
-
-        actionAndSemiStateHolder.addState(this, new ActionToShow(Action.SHOW_EXPEDITION_WHICH_WILL_BE_TAKEN, Expeditions.EXPEDITIONS, id));
-        ActionToShow takeExpeditionAction = players.get(activePlayer).takeExpedition(expedition, drawPile);
-
-        actionAndSemiStateHolder.addState(this, takeExpeditionAction);
-        return actionAndSemiStateHolder;
-    }
-
-    private boolean isGameInOverCheck() {
-        for (Player player : players) {
-            if (player.getInfluencePoints() >= 12) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void cleanTable() {
-        drawPile.getUsedCards(table.removeCards());
-    }
-
-    private void shiftPlayer(Boolean twoShips) {
-        if (twoShips) {
-            for (Player player : players) {
-                player.setCoins(player.getCoins() + player.getJestersCount());
-            }
-            nextActivePlayer(true);
-            phase = Phase.EXPLORING;
-        } else {
-            applyJester();
-            applyAdmiral();
-            nextActivePlayer(false);
-        }
-        try {
-            recalculateShips();
-        } catch (TwoShipsOfSameTypeOnTableException e) {
-            // can not occur
-            e.printStackTrace();
-        }
-        if (getActivePlayerAsPlayer().getAdmiralsCount() > 1) {
-            admiralApplied = false;
-        } else {
-            admiralApplied = true;
-        }
-
-        if (table.getCountOfCards() == 0 && !(Phase.EXPLORING.equals(phase))) {
-            shiftPlayer(false);
-        }
-    }
-
-    private void nextActivePlayer(boolean twoShips) {
-        raiseActivePlayer();
-        if (twoShips) {
-            raisePlayerOnTurn();
-            phase = Phase.EXPLORING;
-            cleanTable();
-        } else {
-            if (activePlayer == playerOnTurn) {
-                cleanTable();
-                phase = Phase.EXPLORING;
-                raisePlayerOnTurn();
-                raiseActivePlayer();
-            } else {
-                phase = Phase.OTHERS_PLAYERS_TRADING;
-            }
-        }
-    }
-
-    private Player getActivePlayerAsPlayer() {
-        return players.get(activePlayer);
-    }
-
-    private void raiseActivePlayer() {
-        activePlayer++;
-        if (activePlayer == playersCount) {
-            activePlayer = 0;
-        }
-    }
-
-    private void endGame() {
-        //TODO
-    }
-
-    private void raisePlayerOnTurn() {
-        if (isGameInOverCheck()) {
-            playerOnTurn++;
-            if (playerOnTurn == playersCount) {
-                playerOnTurn = 0;
-                gameOver = true;
-                endGame();
-            }
-        } else {
-            playerOnTurn++;
-            if (playerOnTurn == playersCount) {
-                playerOnTurn = 0;
-            }
-        }
-    }
-
-    private void recalculateShips() throws TwoShipsOfSameTypeOnTableException {
-        cardsToTake = players.get(activePlayer).getCardsToTake();
-        if (!Phase.OTHERS_PLAYERS_TRADING.equals(phase)) {
-            int skiff = 0, flute = 0, pinace = 0, frigate = 0, galleon = 0;
-            for (Card card : table.getCards()) {
-                switch (card.getCardType()) {
-                    case SKIFF:
-                        skiff += 1;
-                        break;
-                    case FLUTE:
-                        flute += 1;
-                        break;
-                    case PINACE:
-                        pinace += 1;
-                        break;
-                    case FRIGATE:
-                        frigate += 1;
-                        break;
-                    case GALLEON:
-                        galleon += 1;
-                        break;
-                }
-            }
-            if (skiff == 2 || flute == 2 || pinace == 2 || frigate == 2 || galleon == 2) {
-                throw new TwoShipsOfSameTypeOnTableException();
-            }
-
-            int typesCount = skiff + flute + pinace + frigate + galleon;
-            if (typesCount == 4) {
-                cardsToTake += 1;
-            } else if (typesCount == 5) {
-                cardsToTake += 2;
-            }
-        }
-    }
-
-    private ActionAndSemiStateHolder faceTaxes(Card card) throws CloneNotSupportedException {
-        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder();
-        int comparingValue;
-
-        actionAndSemiStateHolder.addState(this, new ActionToShow(Action.SHOW_FACED_TAXES_PRE, Table.TABLE, table.getCards().size() - 1));
-        if (card.getCardType() == CardType.TAX_SWORDS) {
-            comparingValue = 0;
-        } else {
-            comparingValue = 1000;
-        }
-
-        for (Player player : players) {
-            if (player.getCoins() > 11) {
-                player.setCoins(player.getCoins() / 2);
-            }
-
-            if (card.getCardType() == CardType.TAX_SWORDS) {
-                if (player.getSwords() > comparingValue) {
-                    comparingValue = player.getSwords();
-                }
-            } else if (card.getCardType() == CardType.TAX_INFLUENCE) {
-                if (player.getInfluencePoints() < comparingValue) {
-                    comparingValue = player.getInfluencePoints();
-                }
-            }
-        }
-
-        for (Player player : players) {
-            if (card.getCardType() == CardType.TAX_SWORDS && player.getSwords() == comparingValue) {
-                player.setCoins(player.getCoins() + 1);
-            } else if (card.getCardType() == CardType.TAX_INFLUENCE && player.getInfluencePoints() == comparingValue) {
-                player.setCoins(player.getCoins() + 1);
-            }
-        }
-
-        actionAndSemiStateHolder.addState(this, table.faceCard(card));
-        return actionAndSemiStateHolder;
-    }
-
-    private ActionAndSemiStateHolder faceExpedition(Card card) throws CloneNotSupportedException {
-        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder();
-        //show card on table
-        table.addCard(card);
-        ActionToShow actionToShow = new ActionToShow(Action.SHOW_FACED_EXPEDITION_ON_TABLE, Table.TABLE, table.getCards().size() - 1);
-        actionAndSemiStateHolder.addState(this, actionToShow);
-        //show card between expeditions
-        expeditions.addCard(table.removeLastCard());
-        actionToShow = new ActionToShow(Action.SHOW_FACED_EXPEDITION_ON_EXPEDITIONS_PACK, Expeditions.EXPEDITIONS, expeditions.getCards().size() - 1);
-        actionAndSemiStateHolder.addState(this, actionToShow);
-        return actionAndSemiStateHolder;
-    }
-
-    private void applyJester() {
-        if (table.getCountOfCards() == 0) {
-            Player activeP = players.get(activePlayer);
-            activeP.setCoins(activeP.getCoins() * activeP.getJestersCount());
-        }
-    }
-
-    private void cardTypeValidation(Card card) throws InvalidActionException {
-        if (Card.noActionTypes.contains(card.getCardType())) {
-            throw new InvalidActionException("invalid card type");
-        }
+        return getCardFromTableService.pickExpedition(id);
     }
 
     @Override
--- Bachelor project/src/main/java/vsb/cec0094/bachelorProject/gameLogic/services/GetCardFromTableService.java
+++ Bachelor project/src/main/java/vsb/cec0094/bachelorProject/gameLogic/services/GetCardFromTableService.java
@@ -1,13 +1,83 @@
 package vsb.cec0094.bachelorProject.gameLogic.services;
 
+import vsb.cec0094.bachelorProject.exceptions.InvalidActionException;
+import vsb.cec0094.bachelorProject.exceptions.TooExpensiveExpeditionException;
+import vsb.cec0094.bachelorProject.gameLogic.ActionAndSemiStateHolder;
 import vsb.cec0094.bachelorProject.gameLogic.Game;
+import vsb.cec0094.bachelorProject.gameLogic.Phase;
+import vsb.cec0094.bachelorProject.gameLogic.Player;
+import vsb.cec0094.bachelorProject.gameLogic.card.Card;
+import vsb.cec0094.bachelorProject.gameLogic.card.Expedition;
+import vsb.cec0094.bachelorProject.gameLogic.pack.Expeditions;
+import vsb.cec0094.bachelorProject.gameLogic.pack.Table;
+import vsb.cec0094.bachelorProject.models.Action;
+import vsb.cec0094.bachelorProject.models.ActionToShow;
 
 public class GetCardFromTableService {
 
     private Game game;
+    private PlayerSwittchingService playerSwittchingService;
 
     public GetCardFromTableService(Game game) {
         this.game = game;
+        playerSwittchingService = new PlayerSwittchingService(game);
+    }
+
+    public ActionAndSemiStateHolder playerGetCardFromTable(int cardPosition) throws InvalidActionException, TooExpensiveExpeditionException, CloneNotSupportedException {
+        if (Phase.EXPLORING.equals(game.getPhase())) {
+            game.setPhase(Phase.TRADING);
+        }
+        ActionToShow actionToShow = new ActionToShow(Action.GET_CARD, Table.TABLE, cardPosition);
+        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder(game, actionToShow);
+
+        Card card = game.getTable().getCards().remove(cardPosition);
+        cardTypeValidation(card);
+
+        if (!game.getActivePlayer().equals(game.getPlayerOnTurn())) {
+            game.getPlayers().get(game.getPlayerOnTurn()).addCoin();
+        }
+
+        if (Card.shipTypes.contains(card.getCardType())) {
+
+            Player playerDoingAction = game.getPlayers().get(game.getActivePlayer());
+            playerDoingAction.getCoinsFromShip(card);
+            game.getDrawPile().getUsedCard(card);
+
+            game.setCardsToTake(game.getCardsToTake() - 1);
+            if (game.getCardsToTake() == 0) {
+                playerSwittchingService.shiftPlayer(false);
+            }
+            return actionAndSemiStateHolder;
+        } else {
+            Player playerDoingAction = game.getPlayers().get(game.getActivePlayer());
+            playerDoingAction.takeCharacterCard(card, true);
+            playerDoingAction.updateVariables();
+
+            game.setCardsToTake(game.getCardsToTake() - 1);
+            if (game.getCardsToTake() == 0) {
+                playerSwittchingService.shiftPlayer(false);
+            }
+            actionAndSemiStateHolder.addState(game, new ActionToShow(Action.GET_CARD, playerDoingAction.getLogin(), playerDoingAction.getCards().indexOf(card)));
+            return actionAndSemiStateHolder;
+        }
+    }
+
+    public ActionAndSemiStateHolder pickExpedition(int id) throws TooExpensiveExpeditionException, CloneNotSupportedException {
+        ActionAndSemiStateHolder actionAndSemiStateHolder = new ActionAndSemiStateHolder();
+        Expedition expedition = game.getExpeditions().getExpedition(id);
+        game.getPlayers().get(game.getActivePlayer()).canTakeExpedition(expedition); // if not throws exeption
+
+        actionAndSemiStateHolder.addState(game, new ActionToShow(Action.SHOW_EXPEDITION_WHICH_WILL_BE_TAKEN, Expeditions.EXPEDITIONS, id));
+        ActionToShow takeExpeditionAction = game.getPlayers().get(game.getActivePlayer()).takeExpedition(expedition, game.getDrawPile());
+
+        actionAndSemiStateHolder.addState(game, takeExpeditionAction);
+        return actionAndSemiStateHolder;
+    }
+
+    private void cardTypeValidation(Card card) throws InvalidActionException {
+        if (Card.noActionTypes.contains(card.getCardType())) {
+            throw new InvalidActionException("invalid card type");
+        }
     }
 
 }
