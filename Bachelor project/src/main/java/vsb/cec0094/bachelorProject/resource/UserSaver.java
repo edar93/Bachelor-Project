00119bec45bb9c1b6fcd468b5ce8432d651ff0f8
Honza <h.cech@seznam.cz>
2017-03-07 18:42:24 +0100

    refactor userSaver

--- Bachelor project/src/main/java/vsb/cec0094/bachelorProject/resource/UserSaver.java
+++ Bachelor project/src/main/java/vsb/cec0094/bachelorProject/resource/UserSaver.java
@@ -26,11 +26,30 @@ public class UserSaver {
     public void setUser(JoinPoint joinPoint) {
 //        HttpServletRequest httpRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 //        String login = httpRequest.getUserPrincipal().getName();
-        String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        LOGGER.debug("login from request is :" + login);
-        //HttpSecurityUtils.getUsername(httpRequest);
-        //String login = SecurityContextHolder.getContext().getAuthentication().getName();
-        usersProvider.prepareUser(login);
+        String login = null;
+
+//        if(SecurityContextHolder.getContext() == null){
+//            LOGGER.debug("SecurityContextHolder.getContext() is null");
+//        }
+//
+//        if(SecurityContextHolder.getContext().getAuthentication() == null){
+//            LOGGER.debug("SecurityContextHolder.getContext().getAuthentication() is null");
+//        }
+//
+//        if(SecurityContextHolder.getContext().getAuthentication().getName() == null){
+//            LOGGER.debug("SecurityContextHolder.getContext().getAuthentication().getName() is null");
+//        }
+
+        try {
+            login = SecurityContextHolder.getContext().getAuthentication().getName();
+            LOGGER.debug("login from request is :" + login);
+            //HttpSecurityUtils.getUsername(httpRequest);
+            //String login = SecurityContextHolder.getContext().getAuthentication().getName();
+            usersProvider.prepareUser(login);
+        }catch (Exception e){
+            LOGGER.debug("Setting of to user provider fail");
+            e.printStackTrace();
+        }
     }
 
 }
