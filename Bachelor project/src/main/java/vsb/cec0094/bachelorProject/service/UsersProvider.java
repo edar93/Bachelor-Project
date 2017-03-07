ec90f270753600428f87659a1e6fa36c7e3d9d52
Honza <h.cech@seznam.cz>
2017-03-07 11:09:31 +0100

    fix request scope bug

--- Bachelor project/src/main/java/vsb/cec0094/bachelorProject/service/UsersProvider.java
+++ Bachelor project/src/main/java/vsb/cec0094/bachelorProject/service/UsersProvider.java
@@ -3,6 +3,7 @@ package vsb.cec0094.bachelorProject.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import vsb.cec0094.bachelorProject.dao.GameDao;
 import vsb.cec0094.bachelorProject.dao.GamesHolder;
@@ -10,10 +11,7 @@ import vsb.cec0094.bachelorProject.exceptions.NotPlayersTurnException;
 import vsb.cec0094.bachelorProject.gameLogic.GameManipulator;
 import vsb.cec0094.bachelorProject.models.GameInQueue;
 
-@Service
-//@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, value = "request")
-
-//@Scope("request")
+@Component
 public class UsersProvider {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UsersProvider.class);
